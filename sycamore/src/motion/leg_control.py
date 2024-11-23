a9199acdfaac57fc10cee6a2282bedf4eeead4c4
jstwh <weerheimjoost2000@gmail.com>
2024-11-23 19:27:05 +0100

    Added servo support in control

--- sycamore/src/motion/leg_control.py
+++ sycamore/src/motion/leg_control.py
@@ -1,12 +1,19 @@
 from pynput import keyboard
 from leg_ik import Leg
+from adafruit_servokit import ServoKit
 import rerun as rr
 import argparse
 
 
 def main():
-    global x, y, leg, args
+    global x, y, leg, args, kit
     args = parse_args()
+
+    # TODO This needs testing on the real leg!
+    if args.irl:
+        kit = ServoKit(channels=16)
+        kit.servo[0].set_pulse_width_range(500, 2500)
+        kit.servo[1].set_pulse_width_range(500, 2500)
 
     if args.rerun:
         rr.init("rerun_leg_example", spawn=True)
@@ -14,7 +21,10 @@ def main():
     l1 = 100
     l2 = 100
     x = 0
-    y = -140
+    if args.irl:
+        y = 140
+    else:
+        y = -140
 
     leg = Leg(l1, l2)
 
@@ -26,6 +36,10 @@ def update_pos():
     global x, y
     t1, t2 = leg.ja_from_xy(x, y)
     upper, lower = leg.xy_from_ja(t1, t2)
+
+    if args.irl:
+        kit.servo[0].angle = t1
+        kit.servo[1].angle = t2
 
     if args.rerun:
         rr.log(
@@ -64,7 +78,13 @@ def parse_args():
         "--rerun",
         default=False,
         type=bool,
-        help="Flag for enabling rerun, default False.",
+        help="Flag for enabling rerun. Default set to False.",
+    )
+    parser.add_argument(
+        "--irl",
+        default=False,
+        type=bool,
+        help="Flag that can be enabled if the code should work on the real world leg. Default set to False."
     )
     return parser.parse_args()
 
