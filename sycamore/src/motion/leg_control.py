a376348dfb9bb5f5030feb771402da3a586c2af3
jstwh <weerheimjoost2000@gmail.com>
2024-11-23 15:52:57 +0100

    added argument parsing

--- sycamore/src/motion/leg_control.py
+++ sycamore/src/motion/leg_control.py
@@ -1,32 +1,39 @@
 from pynput import keyboard
 from leg_ik import Leg
-import numpy as np
 import rerun as rr
-import rerun.blueprint as rrb
+import argparse
 
-rr.init("rerun_leg_example", spawn=True)
+def main():
+    global x, y, leg, args
+    args = parse_args()
 
-l1 = 100
-l2 = 100
-x = 0
-y = -140
-leg = Leg(l1, l2)
-upper = [[], []]
-lower = [[], []]
+    if args.rerun:
+        rr.init("rerun_leg_example", spawn=True)
+
+    l1 = 100
+    l2 = 100
+    x = 0
+    y = -140
+
+    leg = Leg(l1, l2)
+
+    with keyboard.Listener(on_press=on_press) as listener:
+        listener.join()
 
 def update_pos():
-    global x, y, coords
+    global x, y
     t1, t2 = leg.ja_from_xy(x, y)
     upper, lower = leg.xy_from_ja(t1, t2)
 
-    rr.log(
-        "leg_data",
-        rr.LineStrips3D(
-            [upper, lower],
-            colors=[[255, 0, 0], [0, 255, 0]],
-            labels=["upper leg", "lower leg"],
-        ),
-    )
+    if args.rerun:
+        rr.log(
+            "leg_data",
+            rr.LineStrips3D(
+                [upper, lower],
+                colors=[[255, 0, 0], [0, 255, 0]],
+                labels=["upper leg", "lower leg"],
+            ),
+        )
 
 def on_press(key):
     global x, y
@@ -47,9 +54,15 @@ def on_press(key):
     except AttributeError:
         print(f"Error: special key {key} pressed")
 
-# Set view bounds:
-#rr.send_blueprint(rrb.Spatial3DView(visual_bounds=rrb.VisualBounds3D(x_range=[-l1-l2, l1+l2], y_range=[-l1-l2, l1+l2], z_range=[10, -10])))
+def parse_args():
+    parser = argparse.ArgumentParser(description="Control CLI")
+    parser.add_argument(
+        "--rerun", 
+        default=False, 
+        type=bool,
+        help="Flag for enabling rerun, default False.",
+    )
+    return parser.parse_args()
 
-with keyboard.Listener(
-        on_press=on_press) as listener:
-    listener.join()
+if __name__ == "__main__":
+    main()
