7daddbed41f559c49b4433865f47046eef7e63bd
jstwh <weerheimjoost2000@gmail.com>
2024-11-25 12:34:03 +0100

    increase revolutions

--- sycamore/src/motion/bezier.py
+++ sycamore/src/motion/bezier.py
@@ -8,14 +8,14 @@ def main():
     rr.init("rerun_leg_cicle_example", spawn=True)
 
     radius = 20
-    points = point_on_a_circle(radius, 20)
+    points = point_on_a_circle(radius, 100)
     l1 = 100
     l2 = 100
     leg = Leg(l1, l2)
     update_pos(points, leg)
 
 def update_pos(points, leg):
-    for point in points:
+    for idx, point in enumerate(points):
         t1, t2 = leg.ja_from_xy(point[0], point[1])
         upper, lower = leg.xy_from_ja(t1, t2)
 
@@ -28,13 +28,16 @@ def update_pos(points, leg):
             ),
         )
 
-        time.sleep(0.15)
+        if idx == 1:
+            time.sleep(3)
+        else:
+            time.sleep(0.01)
 
 
 def point_on_a_circle(radius, nr_of_revolutions):
     points = []
     # five circles
-    for theta in np.linspace(0, nr_of_revolutions*math.pi, 100):
+    for theta in np.linspace(0, nr_of_revolutions*math.pi, 100*nr_of_revolutions):
         x = radius*math.sin(theta)
         y = radius*math.cos(theta)-130
         points.append([x, y])
