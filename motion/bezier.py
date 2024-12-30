ca9295c1f888f3cd482955aacaaafa61e96e22d5
jstwh <weerheimjoost2000@gmail.com>
2024-12-30 15:12:40 +0100

    maybe maybe maybe

--- motion/bezier.py
+++ motion/bezier.py
@@ -4,18 +4,16 @@ from leg_ik import Leg
 import rerun as rr
 import time
 
-
 def main():
     rr.init("IK visualization")
     rr.connect_tcp("127.0.0.1:9876")
 
     radius = 20
-    points = point_on_a_circle(radius, 20)
+    points = point_on_a_circle(radius, 100)
     l1 = 100
     l2 = 100
     leg = Leg(l1, l2)
     update_pos(points, leg)
-
 
 def update_pos(points, leg):
     for idx, point in enumerate(points):
@@ -28,7 +26,6 @@ def update_pos(points, leg):
                 [upper, lower],
                 colors=[[255, 0, 0], [0, 255, 0]],
                 labels=["upper leg", "lower leg"],
-                radii=1.5,
             ),
         )
 
@@ -41,12 +38,11 @@ def update_pos(points, leg):
 def point_on_a_circle(radius, nr_of_revolutions):
     points = []
     # five circles
-    for theta in np.linspace(0, nr_of_revolutions * math.pi, 100 * nr_of_revolutions):
-        x = radius * math.sin(theta)
-        y = radius * math.cos(theta) - 130
+    for theta in np.linspace(0, nr_of_revolutions*math.pi, 100*nr_of_revolutions):
+        x = radius*math.sin(theta)
+        y = radius*math.cos(theta)-130
         points.append([x, y])
     return points
-
 
 if __name__ == "__main__":
     main()
